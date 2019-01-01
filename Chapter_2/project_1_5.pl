c3a56ef3b18d2c772399d837b289ef03e9723dca
MortalCatalyst <flebber.crue@gmail.com>
2019-01-01 17:54:41 +1100

    chapter update

--- Chapter_2/project_1_5.pl
+++ Chapter_2/project_1_5.pl
@@ -5,6 +5,7 @@ printf("Please enter the value of x as a whole number: ");
 my $x = <STDIN>;
 chomp $x;
 
-my $answer = 3 * ($x ** 5) + 2 * ($x ** 4) - 5 * ($x ** 3) - ($x ** 2) + 7 * $x - 6;
+my $answer =
+  3 * ( $x**5 ) + 2 * ( $x**4 ) - 5 * ( $x**3 ) - ( $x**2 ) + 7 * $x - 6;
 
 print "The answer is $answer \n";
